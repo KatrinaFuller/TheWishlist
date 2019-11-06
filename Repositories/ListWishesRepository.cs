9b6915e6ea6b4d2c9f57894758ad97f9f70e934d
Katrina Fuller <trinafuller@att.net>
2019-11-06 14:09:26 -0700

    filled out ListWishesRepository

--- Repositories/ListWishesRepository.cs
+++ Repositories/ListWishesRepository.cs
@@ -1,29 +1,47 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using Dapper;
 using TheWistlist.Models;
 
 namespace TheWistlist.Repositories
 {
   public class ListWishesRepository
   {
-    internal IEnumerable<List> GetListWishesByListId(int listId, string userId)
+    private readonly IDbConnection _db;
+    public ListWishesRepository(IDbConnection db)
     {
-      throw new NotImplementedException();
+      _db = db;
+    }
+    public IEnumerable<Wish> GetListWishesByListId(int listId, string userId)
+    {
+      string sql = @"
+      SELECT * FROM listwishes lw
+      INNER JOIN wishes w ON w.id = lw.wishId
+      WHERE(listId = @listId AND lw.userId = @userId)";
+      return _db.Query<Wish>(sql, new { listId, userId });
     }
 
-    internal void CreateListWish(int listId, int wishId, string userId)
+    public void CreateListWish(int ListId, int WishId, string userId)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      INSERT INTO listwishes
+      (listId, wishId, userId)
+      VALUES
+      (@ListId, @WishId, @UserId)";
+      _db.Execute(sql, new { ListId, WishId, userId });
     }
 
-    internal ListWish GetListWishByListIdWishIdAndUserId(ListWish listWish)
+    public ListWish GetListWishByListIdWishIdAndUserId(ListWish lWish)
     {
-      throw new NotImplementedException();
+      string sql = "SELECT * FROM listwishes WHERE wishId = @wishId AND listId = @listId";
+      return _db.QueryFirstOrDefault<ListWish>(sql, lWish);
     }
 
-    internal void RemoveListWish(int id)
+    public void RemoveListWish(int id)
     {
-      throw new NotImplementedException();
+      string sql = "DELETE FROM listwishes WHERE id = @id";
+      _db.Execute(sql, new { id });
     }
   }
 }
