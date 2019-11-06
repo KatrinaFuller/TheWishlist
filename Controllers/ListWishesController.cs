066d8def672e7d5f00bae464575d2e173c62d83f
Katrina Fuller <trinafuller@att.net>
2019-11-06 13:45:32 -0700

    filled out ListWishController

--- Controllers/ListWishesController.cs
+++ Controllers/ListWishesController.cs
@@ -1,7 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using TheWistlist.Models;
+using TheWistlist.Services;
+
 namespace TheWistlist.Controllers
 {
-    public class ListWishesController
+  [ApiController]
+  [Route("api/{controller}")]
+  public class ListWishesController : ControllerBase
+  {
+    private readonly ListWishesService _lws;
+    public ListWishesController(ListWishesService lws)
     {
-        
+      _lws = lws;
     }
+
+    [HttpGet("{listId")]
+    public ActionResult<IEnumerable<List>> GetListWishesByListId(int listId)
+    {
+      try
+      {
+        var userId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_lws.GetListWishesByListId(listId, userId));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<ListWish> CreateListWish([FromBody] ListWish newListWish)
+    {
+      try
+      {
+        newListWish.UserId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_lws.CreateListWish(newListWish));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPut]
+    public ActionResult<string> RemoveListWish([FromBody] ListWish listWish)
+    {
+      try
+      {
+        string userId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_lws.RemoveListWish(listWish, userId));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+  }
 }
--- Services/ListWishesService.cs
+++ Services/ListWishesService.cs
@@ -1,7 +1,23 @@
+using System;
+using TheWistlist.Models;
+
 namespace TheWistlist.Services
 {
-    public class ListWishesService
+  public class ListWishesService
+  {
+    internal object GetListWishesByListId(int listId, string userId)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal object CreateListWish(ListWish newListWish)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object RemoveListWish(ListWish listWish, string userId)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
