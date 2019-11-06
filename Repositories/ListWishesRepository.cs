0c33b9a9343eae9d4ce082bf629c47455fde97a0
Katrina Fuller <trinafuller@att.net>
2019-11-06 14:00:35 -0700

    filled out listWishesService

--- Repositories/ListWishesRepository.cs
+++ Repositories/ListWishesRepository.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Collections.Generic;
+using TheWistlist.Models;
+
 namespace TheWistlist.Repositories
 {
-    public class ListWishesRepository
+  public class ListWishesRepository
+  {
+    internal IEnumerable<List> GetListWishesByListId(int listId, string userId)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal void CreateListWish(int listId, int wishId, string userId)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal ListWish GetListWishByListIdWishIdAndUserId(ListWish listWish)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal void RemoveListWish(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
--- Services/ListWishesService.cs
+++ Services/ListWishesService.cs
@@ -1,23 +1,52 @@
 using System;
+using System.Collections.Generic;
 using TheWistlist.Models;
+using TheWistlist.Repositories;
 
 namespace TheWistlist.Services
 {
   public class ListWishesService
   {
-    internal object GetListWishesByListId(int listId, string userId)
+    public readonly ListWishesRepository _repo;
+    public readonly ListsRepository _lrepo;
+    public readonly WishesRepository _wrepo;
+    public ListWishesService(ListWishesRepository repo, ListsRepository lrepo, WishesRepository wrepo)
     {
-      throw new NotImplementedException();
+      _repo = repo;
+      _lrepo = lrepo;
+      _wrepo = wrepo;
+    }
+    public IEnumerable<List> GetListWishesByListId(int listId, string userId)
+    {
+      return _repo.GetListWishesByListId(listId, userId);
     }
 
-    internal object CreateListWish(ListWish newListWish)
+    public string CreateListWish(ListWish newListWish)
     {
-      throw new NotImplementedException();
+      List list = _lrepo.GetListById(newListWish.ListId);
+      if (list == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      Wish wish = _wrepo.GetWishByWishId(newListWish.WishId);
+      if (wish == null)
+      {
+        throw new Exception("Invalid Id");
+      }
+      _repo.CreateListWish(newListWish.ListId, newListWish.WishId, newListWish.UserId);
+      return "success";
     }
 
-    internal object RemoveListWish(ListWish listWish, string userId)
+    public string RemoveListWish(ListWish listWish, string userId)
     {
-      throw new NotImplementedException();
+      ListWish lWish = _repo.GetListWishByListIdWishIdAndUserId(listWish);
+      int id = lWish.Id;
+      if (lWish == null || lWish.UserId != userId)
+      {
+        throw new Exception("Invalid List Id");
+      }
+      _repo.RemoveListWish(id);
+      return "Removed ListWish";
     }
   }
 }
