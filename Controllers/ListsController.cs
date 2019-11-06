0c6150067b53266ff3a49faa05156d0c254fa005
Katrina Fuller <trinafuller@att.net>
2019-11-05 17:58:00 -0700

    filled out listController

---
+++ Controllers/ListsController.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using TheWistlist.Models;
+using TheWistlist.Services;
+
+namespace TheWistlist.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ListsController : ControllerBase
+  {
+    private readonly ListsService _ls;
+    public ListsController(ListsService ls)
+    {
+      _ls = ls;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<List>> GetListByUserId()
+    {
+      try
+      {
+        var id = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ls.GetListByUserId(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<List> GetListById(int id)
+    {
+      try
+      {
+        return Ok(_ls.GetListById(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<List> CreateList([FromBody] List newList)
+    {
+      try
+      {
+        newList.UserId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_ls.CreateList(newList));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<int> DeleteList(int id)
+    {
+      try
+      {
+        return Ok(_ls.DeleteList(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+  }
+}
---
+++ Services/ListsService.cs
@@ -0,0 +1,28 @@
+using System;
+using TheWistlist.Models;
+
+namespace TheWistlist.Services
+{
+  public class ListsService
+  {
+    internal object GetListByUserId(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object GetListById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object CreateList(List newList)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object DeleteList(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
